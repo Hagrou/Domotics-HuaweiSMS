2f0dfe669b34388904c6b3eb674b02d8b823f081
philippe <philippe.boinot@gmail.com>
2020-05-10 01:08:04 +0200

    Add Test/Send/rec & Main

--- SmsBox.py
+++ SmsBox.py
@@ -1,11 +1,24 @@
 import datetime
-
+import getopt
+import sys
+import time
 from huawei_lte_api.Client import Client
 from huawei_lte_api.Connection import Connection
 from huawei_lte_api.Connection import SetResponseType
 from huawei_lte_api.Connection import GetResponseType
 from huawei_lte_api.enums.sms import BoxTypeEnum
+from huawei_lte_api.exceptions import ResponseErrorException
 
+"""
+160 char de 7 bits ou 70 char Unicode 16
+Long Sms: ensemble de sms a assembler
+
+Message status report: ack de reception activé dans un flag du sms
+Message submission report: ack du serveur de la bonne soumission du sms
+Message delivery report: ack de reception du sms
+
+
+"""
 class SmsBox:
     """Received/Sent Sms Box"""
 
@@ -54,44 +67,92 @@ class SmsBox:
             boxContent = self.huaweiClient.sms.get_sms_list(1, boxType, 20, 0, 0, 1)
         return count
 
-    """ Read all sms """
-    def read(self,boxType: BoxTypeEnum, clean: bool=True):
-        smsStatus = self.huaweiClient.sms.sms_count()
+    """ Read older message """
+    def read(self,boxType: BoxTypeEnum, removeMessage: bool=True) -> (bool,list):
+        try:
+            boxContent = self.huaweiClient.sms.get_sms_list(1, boxType, 1, 0, 1, 1)
+            print(boxContent)
 
-        print(smsStatus)
-        print("***********************************")
-        print("* Inbox: %d (%d unread)" % (int(smsStatus['LocalInbox']), int(smsStatus['LocalUnread'])))
-        print("* Sent:  %d" % (int(smsStatus['LocalOutbox'])))
-        print("* Draft: %d" % (int(smsStatus['LocalDraft'])))
-        # print("* #%d sms in box\n " % nbSms)
-        print("***********************************")
+            if boxContent['Messages'] is None: # no message
+                return (True, None)
+            time.sleep(4) # it can be a big message :/
+            boxContent = self.huaweiClient.sms.get_sms_list(1, boxType, 1, 0, 1, 1)
+            print(boxContent)
+            if isinstance(boxContent['Messages']['Message'],dict): # only one message in box
+                msg = boxContent['Messages']['Message']
+            else:
+                msg = boxContent['Messages']['Message'][0]
 
-        smsList = self.huaweiClient.sms.get_sms_list(1, BoxTypeEnum.LOCAL_INBOX, 50, 0, 0, 1)
+            if removeMessage:
+                self.huaweiClient.sms.delete_sms(msg['Index'])
+            return (True, msg)
+
+        except ResponseErrorException as e:
+            return (False, None)
 
     """ 
         Send a message to a destlist of receiver
         :param numTel: phone number 
         :param msg: sms message
         :param withTimeStamp: insert time stamp at the beginning of the message
+        :param maxRetry: maxRetry count if an error occurs
         :return: (OK, sent msg txt)
         
         Example:
         msgBox.sendAll('+33650284422','Hello All'])
     """
-    def sendTo(self, numTel: str, msg: str, withTimeStamp = False) -> SetResponseType :
-        return self.sendAll([numTel], msg, withTimeStamp)
+    def sendTo(self, numTel: str, msg: str, withTimeStamp = False, maxRetry=10) -> SetResponseType :
+        return self.sendAll([numTel], msg, withTimeStamp,maxRetry)
 
     """ 
         Send a message to a list of receiver
         :param numTel: list of phone number 
         :param msg: sms message
         :param withTimeStamp: insert time stamp at the beginning of the message
+        :param maxRetry: maxRetry count if an error occurs
         :return: (OK, sent msg txt)
         
         Example:
         msgBox.sendAll(['+33650284422','+33740342312'],'Hello All'])
     """
-    def sendAll(self, strNumList: list, msg: str, withTimeStamp = False) -> (SetResponseType, str):
+    def sendAll(self, strNumList: list, msg: str, withTimeStamp = False, maxRetry=10) -> (SetResponseType, str):
+        nbError=0
+        result=''
+
         if withTimeStamp:
             msg=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S-")+msg
-        return (self.huaweiClient.sms.send_sms(strNumList, msg),msg)
+
+        while nbError<maxRetry:
+            try:
+                result=self.huaweiClient.sms.send_sms(strNumList, msg)
+                if result: break
+            except ResponseErrorException as e:
+                nbError +=1
+
+        return (result, msg)
+
+def usage(value):
+    print("SmsBox send or receive Sms")
+    print("\t send <phone> <message>")
+    print("\t rec")
+    exit(value)
+
+def sendSms(phone, msg):
+    smsBox=SmsBox("http://192.168.8.1/")
+    smsBox.sendTo(phone, msg, withTimeStamp = True)
+
+def main(argv):
+    action=None
+    phone=None
+    msg=None
+
+    print(argv)
+
+    if argv[0] == "send":
+        if len(argv) !=3 : usage(1)
+        sendSms(argv[1],argv[2])
+    else:
+        usage()
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
--- test_SmsBox.py
+++ test_SmsBox.py
@@ -2,6 +2,10 @@ from unittest import TestCase
 from SmsBox import *
 import json
 import time
+import hashlib
+
+from random import choice
+from string import ascii_lowercase
 
 class TestSmsBox(TestCase):
     def setUp(self):
@@ -44,14 +48,119 @@ class TestSmsBox(TestCase):
 
     def test_send_withTimeStamp(self):
         self.smsBox.emptyLocal()
-        (s, msg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],"test_message", withTimeStamp = True)
-        self.assertTrue(s == 'OK', self.waitUntil(10, {'LocalUnread':'1','LocalInbox':'1',
+        (result, msg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],"test_message", withTimeStamp = True)
+        self.assertTrue(result, self.waitUntil(10, {'LocalUnread':'1','LocalInbox':'1',
                                                      'LocalOutbox':'1','LocalDraft':'0',
                                                      'LocalDeleted':'0'}))
 
     def test_sendAll(self):
         self.smsBox.emptyLocal()
-        (s, msg) = self.smsBox.sendAll([self.config['sim_card_phone_number'],self.config['sim_card_phone_number']], "test_message")
-        self.assertTrue(s == 'OK', self.waitUntil(10, {'LocalUnread':'2','LocalInbox':'2',
+        (result, msg) = self.smsBox.sendAll([self.config['sim_card_phone_number'],self.config['sim_card_phone_number']], "test_message")
+        self.assertTrue(result, self.waitUntil(10, {'LocalUnread':'2','LocalInbox':'2',
                                                        'LocalOutbox':'1','LocalDraft':'0',
                                                        'LocalDeleted':'0'}))
+
+
+
+    def test_read_with_no_sms(self):
+        self.smsBox.emptyLocal()
+        self.waitUntil(10, {'LocalUnread': '0', 'LocalInbox': '0',
+                            'LocalOutbox': '0', 'LocalDraft': '0',
+                            'LocalDeleted': '0'})
+
+        (result,msg)=self.smsBox.read(BoxTypeEnum.LOCAL_INBOX)
+
+    def test_read_one_msg(self):
+        self.smsBox.emptyLocal()
+        # 604+504
+
+        (result, msg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],
+                                      "[1." + "".join(choice(ascii_lowercase) for i in range(150)) + "]")
+        self.assertTrue(result)
+        self.waitUntil(10, {'LocalUnread': '1', 'LocalInbox': '1',
+                            'LocalOutbox': '1', 'LocalDraft': '0',
+                            'LocalDeleted': '0'})
+        sha1MsgSent = hashlib.sha1()
+        sha1MsgSent.update(bytes(msg,"utf8"))
+        sha1MsgSent.digest()
+
+        (result,msgRec)=self.smsBox.read(BoxTypeEnum.LOCAL_INBOX)
+
+        sha1MsgRec = hashlib.sha1()
+
+        sha1MsgRec.update(bytes(msgRec['Content'], "utf8"))
+        sha1MsgRec.digest()
+        self.assertTrue(sha1MsgSent.digest() == sha1MsgRec.digest())
+
+    def test_read_latest_msg(self):
+        self.smsBox.emptyLocal()
+        (result, latestMsg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],
+                                      "[1." + "".join(choice(ascii_lowercase) for i in range(150)) + "]")
+        self.assertTrue(result)
+        (result, OtherMsg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],
+                                           "[2." + "".join(choice(ascii_lowercase) for i in range(150)) + "]")
+        self.assertTrue(result)
+        self.waitUntil(10, {'LocalUnread': '2', 'LocalInbox': '2',
+                            'LocalOutbox': '2', 'LocalDraft': '0',
+                            'LocalDeleted': '0'})
+        sha1LatestMsg = hashlib.sha1()
+        sha1LatestMsg.update(bytes(latestMsg,"utf8"))
+        sha1LatestMsg.digest()
+
+        (result,msgRec)=self.smsBox.read(BoxTypeEnum.LOCAL_INBOX)
+
+        sha1MsgRec = hashlib.sha1()
+        sha1MsgRec.update(bytes(msgRec['Content'], "utf8"))
+        sha1MsgRec.digest()
+        self.assertTrue(sha1LatestMsg.digest() == sha1MsgRec.digest())
+
+    def test_read_big_msg(self):
+        self.smsBox.emptyLocal()
+        (result, latestMsg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],
+                                      "[1." + "".join(choice(ascii_lowercase) for i in range(300)) + "]")
+        self.assertTrue(result)
+        print(latestMsg)
+
+        self.waitUntil(10, {'LocalUnread': '1', 'LocalInbox': '1',
+                            'LocalOutbox': '1', 'LocalDraft': '0',
+                            'LocalDeleted': '0'})
+        sha1LatestMsg = hashlib.sha1()
+        sha1LatestMsg.update(bytes(latestMsg,"utf8"))
+        sha1LatestMsg.digest()
+
+        (result,msgRec)=self.smsBox.read(BoxTypeEnum.LOCAL_INBOX)
+        print(msgRec['Content'])
+        sha1MsgRec = hashlib.sha1()
+        sha1MsgRec.update(bytes(msgRec['Content'], "utf8"))
+        sha1MsgRec.digest()
+        self.assertTrue(sha1LatestMsg.digest() == sha1MsgRec.digest())
+
+    def test_read_all_msg(self):
+        self.smsBox.emptyLocal()
+
+        sentMsg=[]
+        for i in range(0,9):
+            (result, msg) = self.smsBox.sendTo(self.config['sim_card_phone_number'],
+                                               "[%d:" % i+"".join(choice(ascii_lowercase) for j in range(150)) + "]")
+            sentMsg.insert(i,msg)
+            self.assertTrue(result)
+            time.sleep(1)
+
+        self.waitUntil(10, {'LocalUnread': '10', 'LocalInbox': '10',
+                            'LocalOutbox': '10', 'LocalDraft': '0',
+                            'LocalDeleted': '0'})
+
+        i=0
+        while True:
+            (result,msgRec)=self.smsBox.read(BoxTypeEnum.LOCAL_INBOX)
+            if msgRec is None: break
+            sha1SentMsg = hashlib.sha1()
+            sha1SentMsg.update(bytes(sentMsg[i], "utf8"))
+            sha1SentMsg.digest()
+            sha1RecMsg = hashlib.sha1()
+            sha1RecMsg.update(bytes(msgRec['Content'], "utf8"))
+            sha1RecMsg.digest()
+
+            print(msgRec)
+            self.assertTrue(sha1SentMsg.digest() == sha1RecMsg.digest())
+            i += 1
